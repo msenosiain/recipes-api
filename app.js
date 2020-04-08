04ac5fb3433948f779532cfa883e72d5c22a0651
Miguel Senosiain <msenosiain@gmail.com>
2020-04-08 04:03:04 -0300

    Clean up app

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
@@ -12,21 +10,14 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Configure bodyparser to handle post requests
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
 
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://niles/recipes', { useNewUrlParser: true});
+mongoose.connect('mongodb://niles/recipes', { useNewUrlParser: true });
 var db = mongoose.connection;
 
 // Added check for DB connection
-if(!db)
-    console.log("Error connecting db")
-else
-    console.log("Db connected successfully")
+if (!db) console.log('Error connecting db');
+else console.log('Db connected successfully');
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
